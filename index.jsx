bfe5c54e72e6f9b0d3e99ea02f2877ef02e4893e
Wesley <wesleyvc2110@gmail.com>
2023-05-24 20:56:45 -0300

    Commit 7 - Fatura em HTML

--- index.jsx
+++ index.jsx
@@ -64,7 +64,22 @@ function gerarFaturaStr (fatura, pecas) {
     faturaStr += `Valor total: ${formatarMoeda(calcularTotalFatura(fatura.apresentacoes))}\n`;
     faturaStr += `Créditos acumulados: ${calcularTotalCreditos(fatura.apresentacoes)} \n`;
     return faturaStr;
-} 
+}
+
+function gerarFaturaStr(fatura, pecas) {
+  return(
+  <html>
+  <p> Fatura UFMG </p>
+  <ul>
+  <li>  Hamlet: R$ 650,00 (55 assentos) </li>
+  <li>  As You Like It: R$ 580,00 (35 assentos) </li>
+  <li>  Othello: R$ 500,00 (40 assentos) </li>
+  </ul>
+  <p> Valor total: R$ 1.730,00 </p>
+  <p> Créditos acumulados: 47 </p>
+  </html>
+  )
+}
 
 const faturas = JSON.parse(readFileSync('./faturas.json'));
 const pecas = JSON.parse(readFileSync('./pecas.json'));
